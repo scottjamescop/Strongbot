442043728ed44dee8fcc486522c4d5b0a3640884
scottjamescop <32177251+scottjamescop@users.noreply.github.com>
2025-05-02 19:04:42 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,8 @@ def get_ydl_options():
                 "quiet": True,
                 #"proxy": PROXY,
                 "http_headers": {
-                        "User-Agent": "Mozilla/5.0  (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko>                        "Accept-Language": "en-US,en;q=0.9"
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                        "Accept-Language": "en-US,en;q=0.9"
                 }
         }
 
