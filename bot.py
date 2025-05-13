acb9a64c1120511bb57cb2c9a97ea6bcd4446e24
scottjamescop <32177251+scottjamescop@users.noreply.github.com>
2025-05-13 00:13:39 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def build_ydl_opts():
         "format": "mp4",
         "outtmpl": "%(id)s.%(ext)s",
         "socket_timeout": 20,            # <- longer
-        "cookies": "/root/discord-bot/cookies.txt",
+        "cookies": "/root/discord-bot/www.tiktok.com_cookies.txt",
         "http_headers": {
             "User-Agent":
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
