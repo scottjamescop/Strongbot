eea22f1927e15764dd8794a33da0e95278ba4ac5
scottjamescop <32177251+scottjamescop@users.noreply.github.com>
2025-06-07 00:24:53 -0400

    Update bot.py to better compress large videos

--- bot.py
+++ bot.py
@@ -27,26 +27,77 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-async def compress_video(original_path):
-        compressed_path = original_path.replace(".", "_compressed.")
+async def def compress_video(video_full_path, size_upper_bound, two_pass=True, filename_suffix='cps_'):
 
-        command = [
-                "ffmpeg",
-                "-i", original_path,
-                "-vcodec", "libx264",
-                "-crf", "32",
-                compressed_path
-        ]
-        try:
-                subprocess.run(command, check=True)
+    filename, extension = os.path.splitext(video_full_path)
+    extension = '.mp4'
+    output_file_name = filename + filename_suffix + extension
 
-                if os.path.getsize(compressed_path) <= 8 * 1024 * 1024:
-                        return compressed_path
-                else:
-                        os.remove(compressed_path)
-                        return None
-        except subprocess.CalledProcessError:
-                return None
+    # Adjust them to meet your minimum requirements (in bps), or maybe this function will refuse your video!
+    total_bitrate_lower_bound = 11000
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+    min_video_bitrate = 100000
+
+    try:
+        # Bitrate reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+        probe = ffmpeg.probe(video_full_path)
+        # Video duration, in s.
+        duration = float(probe['format']['duration'])
+        # Audio bitrate, in bps.
+        audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+        # Target total bitrate, in bps.
+        target_total_bitrate = (size_upper_bound * 1024 * 8) / (1.073741824 * duration)
+        if target_total_bitrate < total_bitrate_lower_bound:
+            print('Bitrate is extremely low! Stop compress!')
+            return False
+
+        # Best min size, in kB.
+        best_min_size = (min_audio_bitrate + min_video_bitrate) * (1.073741824 * duration) / (8 * 1024)
+        if size_upper_bound < best_min_size:
+            print('Quality not good! Recommended minimum size:', '{:,}'.format(int(best_min_size)), 'KB.')
+            # return False
+
+        # Target audio bitrate, in bps.
+        audio_bitrate = audio_bitrate
+
+        # target audio bitrate, in bps
+        if 10 * audio_bitrate > target_total_bitrate:
+            audio_bitrate = target_total_bitrate / 10
+            if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+                audio_bitrate = min_audio_bitrate
+            elif audio_bitrate > max_audio_bitrate:
+                audio_bitrate = max_audio_bitrate
+
+        # Target video bitrate, in bps.
+        video_bitrate = target_total_bitrate - audio_bitrate
+        if video_bitrate < 1000:
+            print('Bitrate {} is extremely low! Stop compress.'.format(video_bitrate))
+            return False
+
+        i = ffmpeg.input(video_full_path)
+        if two_pass:
+            ffmpeg.output(i, os.devnull,
+                          **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                          ).overwrite_output().run()
+            ffmpeg.output(i, output_file_name,
+                          **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                          ).overwrite_output().run()
+        else:
+            ffmpeg.output(i, output_file_name,
+                          **{'c:v': 'libx264', 'b:v': video_bitrate, 'c:a': 'aac', 'b:a': audio_bitrate}
+                          ).overwrite_output().run()
+
+        if os.path.getsize(output_file_name) <= size_upper_bound * 1024:
+            return output_file_name
+        elif os.path.getsize(output_file_name) < os.path.getsize(video_full_path):  # Do it again
+            return compress_video(output_file_name, size_upper_bound)
+        else:
+            return False
+    except FileNotFoundError as e:
+        print('You do not have ffmpeg installed!', e)
+        print('You can install ffmpeg by reading https://github.com/kkroening/ffmpeg-python/issues/251')
+        return False
 
 @bot.event
 async def on_ready():
@@ -81,7 +132,7 @@ async def on_message(message):
                         else:
                                 await message.channel.send("compressing video...")
 
-                                compressed_file = await compress_video(video_file)
+                                compressed_file = await compress_video(video_file, 8 * 1000)
 
                                 if compressed_file:
                                         await message.channel.send(file=discord.File(compressed_file))
